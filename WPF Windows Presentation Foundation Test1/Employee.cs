a7144f29a63f79c14c51d3c1f1f1d8cc628d6675
Victor Nicolai Madsen <1078116@ucn.dk>
2020-03-08 16:15:41 +0100

    Add project files.

---
+++ WPF Windows Presentation Foundation Test1/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Windows_Presentation_Foundation_Test1
+{
+    class Employee
+    {
+        public Employee()
+        {
+            Name = "Planke" +
+                   "Madsen";
+        }
+        public Employee(String name)
+        {
+            Name = name;
+        }
+        public String Name;
+    }
+}
---
+++ WPF Windows Presentation Foundation Test1/Shape.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+
+namespace WPF_Windows_Presentation_Foundation_Test1
+{
+    class Shape
+    {
+        public Shape(Canvas canvas)
+        {
+            _canvas = canvas;
+        }
+        public void Draw()
+        {
+            double left = _canvas.ActualWidth * _rand.NextDouble();
+            double top = _canvas.ActualHeight * _rand.NextDouble();
+            _element.SetValue(Canvas.LeftProperty, left);
+            _element.SetValue(Canvas.TopProperty, top);
+            _canvas.Children.Add(_element);
+        }
+        Canvas _canvas;
+        protected UIElement _element;
+        static Random _rand = new Random();
+    }
+    class Circle : Shape
+    {
+
+        public Circle(Canvas canvas)
+            : base(canvas)
+        {
+
+            Ellipse ellipse = new Ellipse();
+            ellipse.Width = 10;
+            ellipse.Height = 10;
+            ellipse.Fill = new SolidColorBrush(Colors.Green);
+            ellipse.Stroke = new SolidColorBrush(Colors.Black);
+            _element = ellipse;
+        }
+    }
+    class Square : Shape
+    {
+        public Square(Canvas canvas)
+            :base(canvas)
+        {
+            Rectangle rect = new Rectangle();
+            rect.Width = 10;
+            rect.Height = 10;
+            rect.Fill = new SolidColorBrush(Colors.Green);
+            rect.Stroke = new SolidColorBrush(Colors.Black);
+            _element = rect;
+
+        }
+    }
+    
+}
