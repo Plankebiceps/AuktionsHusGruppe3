28cf41f02cd3971bc1d0c9ee0a5f076b89ceb79a
Victor <viggo10001@hotmail.com>
2020-04-02 11:27:39 +0200

    Product Klassen fik nogle attributer

---
+++ WcfServiceWithDatabaseAccess/ModelLayer/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace WcfServiceWithDatabaseAccess.ModelLayer
+{
+    [DataContract]
+    public class Product
+    {
+        [DataMember]
+        public int ProductId { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+
+
+
+        public Product(int productId, string name, string description)
+        {
+            ProductId = productId;
+            Name = name;
+            Description = description;
+        }
+        
+    
+    }
+    
+
+    
+}
