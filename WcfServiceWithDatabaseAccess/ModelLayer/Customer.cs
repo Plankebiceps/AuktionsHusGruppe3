5474e8f7cbbbd65d161ee3099d3d6847feca4b13
André Devantier <andr254c@gmail.com>
2020-04-15 14:23:10 +0200

    Customer tilføjet i modellayer, itvivl med Personservice

---
+++ WcfServiceWithDatabaseAccess/ModelLayer/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace WcfServiceWithDatabaseAccess.ModelLayer
+{
+    [DataContract]
+    class Customer
+    {
+        [DataMember]
+        public string Adress { get; set; }
+        [DataMember]
+        public string CustomerEmail { get; set; }
+
+        public Customer(string adress, string customerEmail)
+        {
+            Adress = adress;
+            CustomerEmail = customerEmail;
+        }
+    }
+}
