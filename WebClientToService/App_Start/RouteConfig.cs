08d8bde3d657b67cdfe5580d821df405d946b85b
Kasper Fleischer Andersen <1061098@ucn.dk>
2020-04-30 08:49:11 +0200

    WebClient med RouteConfig, AccountController, WebCustomer, og CustomerLogin html.

--- WebClientToService/App_Start/RouteConfig.cs
+++ WebClientToService/App_Start/RouteConfig.cs
@@ -11,6 +11,12 @@ namespace WebClientToService {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Login",
+                url: "Account/{action}/{id}",
+                defaults: new { controller = "Account", action = "CustomerLogin", id = UrlParameter.Optional });
+            
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
